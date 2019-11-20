85fac21a4bdfc79ba2c7a4d8f7856e389900d757
Vinicius Oliveira <vinicius.olliveira@outlook.com>
2019-11-19 23:01:27 -0200

    Ajustes na estrutura do projeto.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Omnistack is starting now!
         </p>
         <a
           className="App-link"
